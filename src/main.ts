c5f16d075adeff49e10609526d4c4103de419803
SamueleTerraroli <terraroli99@gmail.com>
2025-05-20 14:18:43 +0200

    add: bonus 1

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ if (typeof data === 'string') {
   console.log(data * 2);
 } else if (typeof data === 'boolean') {
   console.log(data ? 'si' : 'no');
+} else if (data === null) {
+  console.log('il dato è vuoto');
+} else if (Array.isArray(data)) {
+  console.log(data.length);
+} else if (data instanceof Promise) {
+  data.then((allert) => console.log(allert))
 } else {
   console.log('tipo non supportato')
 }
